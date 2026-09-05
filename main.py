e07cfdbafab3ba4f4c62821b036377dc0299e51c
mubarak <mubarakokorokey@gmail.com>
2026-09-05 18:31:57 +0100

    updated

--- main.py
+++ main.py
@@ -3,25 +3,33 @@ def calculate_profit(risk, result_r):
 def calculate_loss(risk, result_r):
     return risk * result_r
 
+trade = []
+#a loop for asking user input
+while True:
+    pair = input("Enter the currency pair (or type 'exit' to finish): ")
+    if pair.lower() == 'exit':
+        break
+    direction = input("BUY/SELL: ")
+    risk = float(input("Enter the risk amount: "))
+    result_r = float(input("Enter the result(r): "))
 trades = [
     {
-        "pair": "EURUSD",
-        "direction": "BUY",
-        "risk": 15,
-        "result_r": 3
-    },
-    {
-        "pair": "GBPUSD",
-        "direction": "SELL",
-        "risk": 15,
-        "result_r": -1
+        "pair": pair,
+        "direction": direction,
+        "risk": risk,
+        "result_r": result_r
     }
+
 ]
 
-for trade in trades:
-    if trade["result_r"] > 0:
-        profit = calculate_profit(trade["result_r"], trade["risk"])
+
+
+trade.append(trades)
+
+for i in trades:
+    if i["result_r"] > 0:
+        profit = calculate_profit(i["result_r"], i["risk"])
     else:
-        loss = calculate_loss(trade["result_r"], trade["risk"])
+        loss = calculate_loss(i["result_r"], i["risk"])
 print(f"Total Profit: {profit}")
 print(f"Total Loss: {loss}")

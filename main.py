9444682bca4df9eb7c9e65de14703785327c3eb2
mubarak <mubarakokorokey@gmail.com>
2026-09-05 23:49:45 +0100

    write and read with json

--- main.py
+++ main.py
@@ -1,35 +1,90 @@
-def calculate_profit(risk, result_r):
+import json
+import os
+from datetime import date
+
+
+def calculate_pnl(risk, result_r):
     return risk * result_r
-def calculate_loss(risk, result_r):
-    return risk * result_r
-
-trade = []
-#a loop for asking user input
-while True:
-    pair = input("Enter the currency pair (or type 'exit' to finish): ")
-    if pair.lower() == 'exit':
-        break
-    direction = input("BUY/SELL: ")
-    risk = float(input("Enter the risk amount: "))
-    result_r = float(input("Enter the result(r): "))
-trades = [
-    {
-        "pair": pair,
-        "direction": direction,
-        "risk": risk,
-        "result_r": result_r
-    }
-
-]
 
 
+def main():
+    print("Welcome to the Trade PnL Journal!")
+    journal()
 
-trade.append(trades)
 
-for i in trades:
-    if i["result_r"] > 0:
-        profit = calculate_profit(i["result_r"], i["risk"])
+def journal():
+    today = date.today()
+   #load trades
+    if os.path.exists("trades.json"):
+        with open("trades.json", "r") as file:
+            trades = json.load(file)
     else:
-        loss = calculate_loss(i["result_r"], i["risk"])
-print(f"Total Profit: {profit}")
-print(f"Total Loss: {loss}")
+        trades = []
+
+    #a loop for asking user input
+    while True:
+        pair = input("Enter the currency pair: ")
+        direction = input("BUY/SELL: ")
+        risk = float(input("Enter the risk amount: "))
+        result_r = float(input("Enter the result(r): "))
+        
+        trade = {
+            "date": str(today),
+            "pair": pair,
+            "direction": direction,
+            "risk": risk,
+            "result_r": result_r
+        }
+        trades.append(trade)
+
+        again = input("Add another trade? (y/n): ")
+        if again.lower() != 'y':
+            break
+
+    
+    #save all trades
+    with open("trades.json", "w") as file:
+        json.dump(trades, file, indent=4)
+
+    print("Trades saved successfully!")
+
+    total_profit= 0
+    total_r = 0
+    total_loss = 0
+    breakevens = 0
+    net_pnl = 0
+    wins = 0
+    losses = 0
+
+    for i in trades:
+        result = calculate_pnl(i["risk"], i["result_r"])
+        total_r += i["result_r"]
+        net_pnl += result
+
+        if i["result_r"] > 0:
+            total_profit += result
+            wins += 1
+
+        elif i["result_r"] == 0:
+            breakevens +=1
+
+        else:
+            total_loss += result
+            losses += 1
+
+    total_trades = len(trades)
+
+    print("\n----- TRADING SUMMARY -----")
+    print(f"Total Trades: {total_trades}")
+    print(f"Wins: {wins}")
+    print(f"Losses: {losses}")
+    print(f"Breakevens: {breakevens}")
+    print(f"Profit: ${total_profit:.2f}")
+    print(f"Loss: ${total_loss:.2f}")
+    print(f"Total R: {total_r:.2f}R")
+    print(f"Net PnL: ${net_pnl:.2f}")
+
+
+
+if __name__ == "__main__":
+    main()
